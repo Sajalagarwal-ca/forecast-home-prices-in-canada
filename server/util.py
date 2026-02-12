191abf445893631df14db6cfd99084a16c5104a0
Sajal AGarwal <sajal.agarwal.001.ca@gmail.com>
2026-02-12 07:11:57 -0500

    saved after clearing outputs

--- server/util.py
+++ server/util.py
@@ -27,13 +27,13 @@ def load_saved_artifacts():
     global  __data_columns
     global __locations
 
-    with open("./server/artifacts/columns.json", "r") as f:
+    with open("E:/Projects/forecast home prices in canada/server/artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[2:]  # first 2 columns are bed, bath
 
     global __model
     if __model is None:
-        with open('./server/artifacts/canada_home_prices_model.pickle', 'rb') as f:
+        with open('E:/Projects/forecast home prices in canada//server/artifacts/canada_home_prices_model.pickle', 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
